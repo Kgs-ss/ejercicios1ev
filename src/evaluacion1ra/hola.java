7cb81789df0f9632a87b1ea6ad284f498386fefb
Kgs-ss <simoncursosweb@gmail.com>
2020-10-21 17:25:51 +0200

    ej clase

--- src/evaluacion1ra/hola.java
+++ src/evaluacion1ra/hola.java
@@ -7,6 +7,8 @@ public class hola {
 
 		
 		System.out.print("Hola");
+		
+		
 	}
 
 }
---
+++ src/unidad2/ej1.java
@@ -0,0 +1,18 @@
+package unidad2;
+
+public class ej1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double num=1234;
+		double num2= 532;
+		double result = num / num2;
+		
+		System.out.println(1234d /532d); //la d indica entero para double f
+		System.out.printf("%015.2f", result); //el 15 indica los numeros de la parte entera, el 0 rellena con 0s y si ponemos un -alinea a la izquierda
+		
+		//system clase, out objeto, print metodo
+	}
+
+}
