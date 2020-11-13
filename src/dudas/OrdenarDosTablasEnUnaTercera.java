d218c66b824f887af9c07e2f7d0a6742aa2566e6
Kgs-ss <simoncursosweb@gmail.com>
2020-11-13 14:20:24 +0100

    s

--- src/dudas/OrdenarDosTablasEnUnaTercera.java
+++ src/dudas/OrdenarDosTablasEnUnaTercera.java
@@ -18,52 +18,66 @@ public class OrdenarDosTablasEnUnaTercera {
 		
 		boolean crecienteA=true;
 		boolean crecienteB=true;
-		//boolean crecienteC=false;
-
+		
+		
 		for (int i=0;i<a.length;i++) {
 			
-			if (i==0){				//la primera vez se introduce normal rellenamos tablas
+			if (i==0){				
 				
-			System.out.println("introduce un numero para rellenar la primera tabla");
-			
-			a[i]=sc.nextInt();
-			
-			System.out.println("introduce un numero para rellenar la segunda tabla");
-			
-			b[i]=sc.nextInt();
-		
+				System.out.println("introduce el primer numero de la primera tabla");
+				
+				a[i]=sc.nextInt();
 			}
-			else if(i > 0) {		//a partir de la segunda comparamos con la primera
+			
+			else if (i > 0) {
 			
 				do {
+					
 					System.out.println("introduce un numero para rellenar la primera tabla");
 					a[i]=sc.nextInt();
 				
-					if(a[i-1]>=a[i]) {					//con esto aseguramos que el numero introducido sea mayor que el anterior
+					if(a[i-1]>=a[i]) {					
 						
 						System.out.println("Debes introducir un numero mayor que el anterior");
-						crecienteA=false;									//si no lo es avisamos
+						crecienteA=false;									
 					}
 					else
 						crecienteA=true;
 			
 				}while(!crecienteA);
 			
-				do{										//aqui lo mismo para el rellenar el segundo array
-				
-					System.out.println("introduce un numero para rellenar la segunda tabla");
-					b[i]=sc.nextInt();
-				
-					if(b[i-1]>=b[i]) {
-					
-						System.out.println("Debes introducir un numero mayor que el anterior");
-						crecienteB=false;
-					}
-					else
-						crecienteB=true;
-					}while(!crecienteB);
-				}
+			}
+		}
+		
+		for (int i=0;i<b.length;i++) {
 			
+				if (i==0){				
+				
+				System.out.println("introduce el primer numero de la segunda tabla");
+				
+				b[i]=sc.nextInt();
+				
+				}
+				
+				else if (i > 0) {
+			
+					do {
+						
+						System.out.println("introduce un numero para rellenar la segunda tabla");
+						b[i]=sc.nextInt();
+					
+						if(b[i-1]>=b[i]) {					
+							
+							System.out.println("Debes introducir un numero mayor que el anterior");
+							crecienteB=false;									
+						}
+						else
+							crecienteB=true;
+				
+					}while(!crecienteB);
+			
+				}
+
 		}
 		
 		for (int k=0;k<a.length;k++) {		//mostramos los dos bucles
@@ -77,29 +91,26 @@ public class OrdenarDosTablasEnUnaTercera {
 				System.out.println("c" + j +": " +c[j] );
 							
 			}
-		for (int l=0;l<b.length;l++) { 			//*********************** recorre el array b
+		for (int l=0;l<b.length;l++) { 			//*************************recorre el array b
 			
 			for (int m=0;m<c.length;m++) {		//recorre el array c
 				
 				
 				if (b[l]<=c[m]) {				//comprueba si el valor de b[l] es menor que alguno de los valores de c[]
 					
-					for (int t = c.length-1; t > m; t--) {		//mueve una posicion el array dejando un valor "libre", duplicado para eliminarlo
+					for (int t = c.length-2; t > m; t--) {		//mueve una posicion el array dejando un valor "libre", duplicado para eliminarlo
 						
 						c[t+1] = c[t];
-					}
-					
-					c[m] = b[l];				//una vez ha movido lo necesario introduce en la posicion de c[] el valor de b[]		
+					}			
+									//una vez ha movido lo necesario introduce en la posicion de c[] el valor de b[]		
 					
 				}
-				
-				else
 					
-					c[m] = b[l];			//si no hay ningun valor en c[] menor que b[l] lo introduce al final de c 
+					c[m] = b[l];			//******************************si no hay ningun valor en c[] menor que b[l] lo introduce al final de c 
 
 			}
 			
-		}									//******************************
+		}
 		
 		for (int x=0;x<c.length;x++) {  //mostramos el resultado final
 			
@@ -108,7 +119,4 @@ public class OrdenarDosTablasEnUnaTercera {
 		
 		sc.close();
 	}
-
-
-
 }
