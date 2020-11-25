96edfdb18895aaa06bcf6a07a212246df4ba283c
Kgs <Kgs@DESKTOP-EDEM0K4>
2020-11-25 22:12:48 +0100

    -

--- src/dudas/robot.java
+++ src/dudas/robot.java
@@ -9,52 +9,45 @@ public class robot {
 		// TODO Auto-generated method stub
 		
 		int eleccion = 0;
-		String pelo="";
-		String ojos = "";
-		String nariz = "";
-		String boca = "";
-		String barbilla = "";
+		//declaramos las variables con el primer valor que nos interesa
+		String pelo="pelo";		
+		String ojos = "ojos";
+		String nariz = "nariz";
+		String boca = "boca";
+		String barbilla = "barbilla";
 			
-		String [][] v = {{"WWWWWW","\\\\\\////","@@@@@@@@@","__#####__","77777777"},
+		String [][] v = {{"WWWWWW","\\\\\\///////","@@@@@@@@@","__#####__","77777777"},
 					   {"|  o o  |","| (. .) |", "|  O O  |","| \\  /  |"},
 					   {"|   G   |","|   J   |", "G   k   G","!   L   !"},
 					   {"@@@---@@@","|  ___  |", "|  ---  |","|  uuu  |"},
 					   {"_________", "@@@@@@@@@","LLLLLLLLL","WWWWWWWWW"}
 		};
 		
-		String [] parte = {"pelo", "ojos","nariz","boca", "barbilla"}; 
-		
-		for (int i = 0; i < v.length;i++) {
-			
-			
-			for (int j=0;j < v[j].length;j++) {
+		//array con las variables para trabajar con ellas
+		String [] variables = {pelo, ojos, nariz, boca, barbilla};
+		//recorremos los arrays principales
+		for (int i = 0; i < v.length;i++) {	
+			//recorremos el interno mostrando los valores almacenados
+			for (int j=0;j < v[i].length;j++) {
 				
 				System.out.println(j + " : " + v[i][j] );
 			}
-			
-			System.out.println("Seleccione " + parte[i]);
+			//pedimos seleccion, variables[i] contiene el valor asignado al principio, lo sobreescribimos
+			System.out.println("\nSeleccione " + variables[i] + "\n");
 		
 			eleccion = sc.nextInt();
 			
-			if (i == 0)
-				pelo = v[i][eleccion];
-			if (i == 1)
-				ojos = v[i][eleccion];
-			if (i == 2)
-				nariz = v[i][eleccion];
-			if (i == 3)
-				boca = v[i][eleccion];
-			if (i == 4)
-				barbilla = v[i][eleccion];
+			variables[i] = v[i][eleccion];
 			
 		}
 		
 		System.out.println();
-		System.out.println("  " + pelo);
-		System.out.println("  " + ojos);
-		System.out.println("  " + nariz);
-		System.out.println("  " + boca);
-		System.out.println("  " + barbilla);
+		
+		//finalmente imprimimos
+		for (int k=0; k < variables.length;k++) {
+		
+		System.out.println("  " + variables[k]);
+		}
 		sc.close();
 	}
 }
